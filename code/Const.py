779f772b21208cc6037dfc6380329d752acee41b
gaiaramos <gaialatshvia@gmail.com>
2024-12-12 21:18:36 -0300

    Creating Players and Enemies Moving players and enemies

--- code/Const.py
+++ code/Const.py
@@ -1,11 +1,15 @@
 # Cores
+import pygame
+
 from code.entity import Entity
 
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_YELLOW = (255, 255, 0)
 
-# Entity
+# Eventos
+EVENT_ENEMY = pygame.USEREVENT + 1
+
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -14,15 +18,36 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Player1': 3,
+    'Player2': 3,
+    #'Player3': 3,
+    'Enemy1': 2,
+    'Enemy2': 1,
 }
 
+# Player
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
+
 # Definição perfil
-MENU_OPTION = ('NEW GAME 1P',
+MENU_OPTION = ('NEW GAME',
                'LEVEL 1',
                'LEVEL 2',
                'LEVEL 3',
                'SCORE',
                'EXIT')
+
+# Spawn
+SPAWN_TIME = 4000
+
 # Tamanhos
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
--- code/player.py
+++ code/player.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.key
 
-from Entity import Entity
+from code.Const import ENTITY_SPEED, WIN_HEIGHT, WIN_WIDTH, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, \
+    PLAYER_KEY_RIGHT
+from code.entity import Entity
 
 
 class Player(Entity):
-    def __init__(self):
-        pass
-
-    def update(self, ):
-        pass
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
+        pressed_key =  pygame.key.get_pressed()
+        if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 0: # Andar para cima
+            self.rect.centery -= ENTITY_SPEED[self.name]
+        if pressed_key[PLAYER_KEY_DOWN] and self.rect.bottom < WIN_HEIGHT: # Andar para baixo
+            self.rect.centery += ENTITY_SPEED[self.name]
+        if pressed_key[PLAYER_KEY_LEFT] and self.rect.left > 0: # Andar para esquerda
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+        if pressed_key[PLAYER_KEY_RIGHT] and self.rect.right < WIN_WIDTH: # Andar para direita
+            self.rect.centerx += ENTITY_SPEED[self.name]
+
         pass
